6aef368750bee437cb3d96e5ee8745722296338a
Sravanikonapalli <konapallisravani@gmail.com>
2025-02-26 14:09:29 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Itinerary from "./components/Itinerary";
-import Pricing from "./components/Pricing";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
-import Gallery from "./components/Gallery";
 import TermsAndConditions from "./components/TermsAndConditions";
-function App() {
+const App=()=>{
   return (
     <div>
       <Navbar />
